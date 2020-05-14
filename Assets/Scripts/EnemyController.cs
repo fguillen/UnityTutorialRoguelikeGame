53a4a5357e71c63927bbb78a3a08fd89751057ca
Fernando Guillen <fguillen.mail@gmail.com>
2020-05-14 22:20:48 +0200

    Enemy moving!!

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+  public float moveSpeed;
+  public Rigidbody2D theRB;
+  private Vector3 moveDirection;
+  public float rangeToChasePlayer;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+        
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (Vector3.Distance(transform.position, PlayerController.instance.transform.position) < rangeToChasePlayer)
+    {
+      moveDirection = PlayerController.instance.transform.position - transform.position;
+      
+    } else
+    {
+      moveDirection = Vector3.zero;
+    }
+
+    moveDirection.Normalize();
+    theRB.velocity = moveDirection * moveSpeed;
+  }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+  public static PlayerController instance;
+
   public float moveSpeed;
   private Vector2 moveInput;
   public Rigidbody2D theRB;
@@ -16,6 +18,11 @@ public class PlayerController : MonoBehaviour
   public Transform gunArm;
 
   private Camera theCam;
+
+  private void Awake()
+  {
+    instance = this;
+  }
 
   // Start is called before the first frame update
   void Start()
