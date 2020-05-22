44d08006e0b913b1236ed9a1e4dc54f5de4ae5d3
Fernando Guillen <fguillen.mail@gmail.com>
2020-05-22 20:12:56 +0200

    Menus (version "We celebrate our victories")

---
+++ Assets/Scripts/VictoryScreen.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VictoryScreen : MonoBehaviour
+{
+  public float waitToShowPressKeyMessageSeconds = 2f;
+  public GameObject pressKeyText;
+  public string sceneToLoad;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+        
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if(waitToShowPressKeyMessageSeconds > 0)
+    {
+      waitToShowPressKeyMessageSeconds -= Time.deltaTime;
+      if(waitToShowPressKeyMessageSeconds <= 0)
+      {
+        pressKeyText.SetActive(true);
+      }
+    }
+
+    if(waitToShowPressKeyMessageSeconds <= 0 && Input.anyKeyDown)
+    {
+      SceneManager.LoadScene(sceneToLoad);
+    }
+    
+  }
+}
