1197996d834e85d22db24eea1d0c37c9a286caa3
Fernando Guillen <fguillen.mail@gmail.com>
2020-05-24 23:02:56 +0200

    Generating Procedural Levels (version "Nothing here, nothing there.. pam! a room")

---
+++ Assets/LevelGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+  public GameObject templateRoom;
+  public Color firstRoomColor;
+  public Color lastRoomColor;
+  public Transform generatorPoint;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    Instantiate(templateRoom, generatorPoint.position, generatorPoint.rotation).GetComponent<SpriteRenderer>().color = firstRoomColor;
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    
+  }
+}
